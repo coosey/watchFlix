d4ce726a14f42bab62511bbca6458074e11e5314
matthewnguyen <itsmatthewnguyen@gmail.com>
2022-02-15 15:08:36 -0800

    working on webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.join(__dirname, "/client/src/index.js"),
+  output: {
+    path: path.join(__dirname, "/client/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: ["css-loader", "sass-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+}
