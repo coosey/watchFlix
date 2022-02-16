dcafbda836b70e76906f599193872dceaa80bfc2
matthewnguyen <itsmatthewnguyen@gmail.com>
2022-02-15 22:51:50 -0800

    added Movies Component folder

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
 import NavBar from "./NavBar/NavBar.jsx";
+import Movies from "./Movies/Movies.jsx";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Movies />
     </div>
   )
 }
---
+++ client/src/Components/Movies/Movies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Styles/Movies.scss";
+
+const Movies = () => {
+  return (
+    <div>
+      <h2>Top 10 Movies</h2>
+    </div>
+  )
+}
+
+export default Movies;
