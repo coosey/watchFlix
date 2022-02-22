22326c7f3cd441f20a324b1aa4c28f68a0f42a94
matthewnguyen <itsmatthewnguyen@gmail.com>
2022-02-22 14:50:21 -0800

    attempting to fix avatar img sizing

--- client/src/Components/Modal/MovieModal.jsx
+++ client/src/Components/Modal/MovieModal.jsx
@@ -20,20 +20,18 @@ const MovieModal = ({ modalVisible, hideModal, credits, details, setMovieDetails
         <h1>{details.title}</h1>
         <div className="overview">{details.overview}</div>
         {credits.map((cast) => (
-          <div key={cast.cast_id}>
-            <div
-              className="movie-cast"
-              key={cast.cast_id}
-            >
-              <img
-                style={{ width: 50, height: 50, borderRadius: "50%"}}
-                src={
-                  cast.profile_path ? `https://image.tmdb.org/t/p/original${cast.profile_path}` :
-                  "https://icon-library.com/images/no-profile-pic-icon/no-profile-pic-icon-11.jpg"
-                }
-              />
-              <div>{cast.name} - "{cast.character}"</div>
-            </div>
+          <div
+            className="movie-cast"
+            key={cast.cast_id}
+          >
+            <img
+              src={
+                cast.profile_path ? `https://image.tmdb.org/t/p/original${cast.profile_path}` :
+                "https://icon-library.com/images/no-profile-pic-icon/no-profile-pic-icon-11.jpg"
+              }
+              alt={`${cast.name}`}
+            />
+            <div>{cast.name} - "{cast.character}"</div>
           </div>
         ))}
       </section>
